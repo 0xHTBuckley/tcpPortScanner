818dfe018670d34332979c9527cbc56cba1fd9ab
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-04-27 01:02:12 +0800

    Implemented non-functioning prototype of ping sweep scan, added a to do list, and error checking.

--- port_scanner.py
+++ port_scanner.py
@@ -2,19 +2,39 @@
 
 import socket
 import sys
+import subprocess
+
+#To do:
+#For loop to rotate through host bit of pingsweep
+#Figure out how to either use ICMP to get a changing return value / use a bash command or script to ping Sweep a subnet
+#Put in .slice to alter IP address to be inputted as "x.x.x.{hostBit}"
 
 def usageMsg():
-    print(f"""\nUsage: python3 {sys.argv[0]} [ipAddress] [-sWP]
-            
-            Command Summary: 
-                [-help | -h]     Provides a short guide of how to use the tooling
-                [-sWP]      Scan well known port range (1 - 1023)""")
+    print(f"\nUsage: {sys.argv[0]} [ipAddress] [-sWP]")
 
-if len(sys.argv) < 2 or sys.argv[1] == "-help":
+def helpMsg():
+    print(f"""\nCommand Summary: 
+                [-help | -h]     Provides a short guide of how to use the tooling
+                [-tS]      Scan all ports through TCP connections""")
+
+if len(sys.argv) < 2:
     usageMsg()
     quit()
+if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    helpMsg()
+    quit()
 
-def scanPorts():
+def pingSweep(): 
+        alteredIP = .slice#####
+        for hostBit in range(1,255):
+            # Returns a 0 on success | 1 on failure
+            ping = subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2", sys.argv[1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait()
+            if ping == 0:
+                print('it worked')
+            if ping != 0:
+                print("It failed")
+
+def tcpScan():
     host = sys.argv[1]
     for port in range(0, 65536):
         try:
@@ -28,5 +48,6 @@ def scanPorts():
             quit("Exiting!")
         _socketLoop.close()
 
-scanPorts()
+pingSweep()
+#tcpScan()
 
