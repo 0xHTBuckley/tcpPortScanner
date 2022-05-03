69087d1fb3ab5ec304fb53068b3d264f91cff37f
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-05-03 22:52:11 +0800

    Added a functioning stealth SYN scanning function, altered the usage output.

--- port_scanner.py
+++ port_scanner.py
@@ -4,19 +4,22 @@ import socket
 from ipaddress import ip_network
 import sys
 import subprocess
+import random
+from scapy.all import sr1, TCP, IP
 
 #To do:
 #For loop to rotate through host bit of pingsweep
-#Figure out how to either use ICMP to get a changing return value / use a bash command or script to ping Sweep a subnet
-#Put in to alter IP address to be inputted as "x.x.x.{hostBit}"
+#Figure out how to either use ICMP to get a changing return value / use a bash command or script to ping Sweep a subnet #POTENTIALLY USE SCAPY ICMP PACKET **WOULD REQUIRE CHANGE FROM subprocess
+#REFURBISH AND REFINE THE SYNSTEALTH FUNCTION
 
 def usageMsg():
-    print(f"\nUsage: {sys.argv[0]} [ipAddress] [-htCS] [--help]")
+    print(f"\nUsage: {sys.argv[0]} [ipAddress] [-htCSpS] [--help]")
 
 def helpMsg():
     print(f"""\nCommand Summary: 
                 [--help | -h]     Provides a short guide of how to use the tooling
-                [-tCS]      Scan all ports through TCP connections""")
+                [-tCS]      Scan all ports through TCP connections
+                [-pS]       A ping sweep through a""")
 
 # Usage and help checkers
 if len(sys.argv) < 2:
@@ -31,11 +34,29 @@ def pingSweep():
             hostIP = ip_network(sys.argv[1], strict=False)
             for addr in hostIP:
                 # Returns a 0 on success | 1 on failure
-                ping = subprocess.Popen(["ping", "-c", "1", "-n", "-W", "0.035", str(addr)], stdout=subprocess.PIPE).wait()
+                ping = subprocess.Popen(["ping", "-c", "1", "-n", "-W", "0.025", str(addr)], stdout=subprocess.PIPE).wait()
                 if ping == 0:
                         print(f"{addr} : Detected as online")
         except:
             print("f")
+
+def tcpSynStealthScan():
+    for dstport in range(0, 65536):
+        srcport = random.randint(1, 65535)
+        if dstport == srcport:
+            srcport =+ 1
+        scan = sr1(IP(dst = sys.argv[1])/TCP(sport = srcport, dport = dstport, flags = "S"), verbose = 0, timeout = 0.025)
+        if scan == None:
+            print(f"{dstport} closed")
+            continue
+        try:
+            if scan.getlayer(TCP).flags == "SA":
+                print(f"{dstport} : Open")
+                sr1(IP(dst = sys.argv[1])/TCP(sport = srcport, dport = dstport, flags = "R"), verbose = 0, timeout = 0.025)
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt!")
+            quit("Exiting!")
+    print("DONE")
 
 
 def tcpFullConnectScan():
@@ -45,13 +66,16 @@ def tcpFullConnectScan():
             _socketLoop = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             _socketLoop.settimeout(0.01)
             if not _socketLoop.connect_ex((host, port)):
-                print(f"Port {port} is open")
-                _socketLoop.close()    
+                print(f"Port {port} is open")  
+            _socketLoop.close()
         except KeyboardInterrupt:
             print("\nYou have pressed CTRL+C")
             quit("Exiting!")
-        _socketLoop.close()
+        except socket.error:
+            print("\nUnable to make a connection")
+            quit("Exiting!")
 
-pingSweep()
+#pingSweep()
 #tcpFullConnectScan()
+tcpSynStealthScan()
 
