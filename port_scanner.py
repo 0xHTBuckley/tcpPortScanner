4f48253c805e57264cd89a9be057c69190b4a256
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-05-09 11:05:13 +0800

    Added XMAS scan, a hostname sweeping function.

--- port_scanner.py
+++ port_scanner.py
@@ -8,14 +8,18 @@ import random
 from scapy.all import sr1, TCP, IP
 
 #To do:
-#For loop to rotate through host bit of pingsweep
-#Figure out how to either use ICMP to get a changing return value / use a bash command or script to ping Sweep a subnet #POTENTIALLY USE SCAPY ICMP PACKET **WOULD REQUIRE CHANGE FROM subprocess
+#Use a bash command or script to ping Sweep a subnet #POTENTIALLY USE SCAPY ICMP PACKET **WOULD REQUIRE CHANGE FROM subprocess
 #REFURBISH AND REFINE THE SYNSTEALTH FUNCTION
+#CREATE A REPLICABLE SCAN FOR XMAS // SPEED UP THROUGH THREADING TEST FOR IMPROVEMENT
+#TRY TO REDUCE THE try: except: BLOCKS TO A MENU OR FLAG
+#IMPLEMENT ARGPARSE LIBRARY OR OWN TYPE
+#IMPLEMENT FIN, NULL, ACK FIREWALL DISCOVERY, WINDOW SCAN
+#TEST THREADING TO SEE IF IMPROVEMENT OR NOT
 
 def usageMsg():
     print(f"\nUsage: {sys.argv[0]} [ipAddress] [-htCSpS] [--help]")
 
-def helpMsg():
+def manMsg():
     print(f"""\nCommand Summary: 
                 [--help | -h]     Provides a short guide of how to use the tooling
                 [-tCS]      Scan all ports through TCP connections
@@ -26,8 +30,19 @@ if len(sys.argv) < 2:
     usageMsg()
     quit()
 if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    helpMsg()
+    manMsg()
     quit()
+
+def hostnameSweep():
+    hostIP = ip_network(sys.argv[1], strict=False)
+    for addr in hostIP:
+        try:
+            scanResult = socket.gethostbyaddr(str(addr))
+            if type(scanResult) == tuple:
+                print(scanResult[0])
+        except socket.herror:
+            continue
+
 
 def pingSweep(): 
         try:
@@ -37,27 +52,37 @@ def pingSweep():
                 ping = subprocess.Popen(["ping", "-c", "1", "-n", "-W", "0.025", str(addr)], stdout=subprocess.PIPE).wait()
                 if ping == 0:
                         print(f"{addr} : Detected as online")
-        except:
-            print("f")
+        except KeyboardInterrupt:
+            print()
 
 def tcpSynStealthScan():
     for dstport in range(0, 65536):
         srcport = random.randint(1, 65535)
         if dstport == srcport:
-            srcport =+ 1
-        scan = sr1(IP(dst = sys.argv[1])/TCP(sport = srcport, dport = dstport, flags = "S"), verbose = 0, timeout = 0.025)
-        if scan == None:
-            print(f"{dstport} closed")
-            continue
+            srcport += 1
         try:
+            scan = sr1(IP(dst = sys.argv[1])/TCP(sport = srcport, dport = dstport, flags = "S"), verbose = 0, timeout = 0.01)
+            if scan == None:
+                continue
             if scan.getlayer(TCP).flags == "SA":
                 print(f"{dstport} : Open")
-                sr1(IP(dst = sys.argv[1])/TCP(sport = srcport, dport = dstport, flags = "R"), verbose = 0, timeout = 0.025)
+                sr1(IP(dst = sys.argv[1])/TCP(sport = srcport, dport = dstport, flags = "R"), verbose = 0, timeout = 0.01)
         except KeyboardInterrupt:
-            print("Keyboard Interrupt!")
-            quit("Exiting!")
-    print("DONE")
+            quit("\nKeyboard Interrupt, exiting!")
 
+def tcpXmasScan():
+    for dstport in range(0, 65536):
+        srcport = random.randint(1, 65535)
+        if dstport == srcport:
+            srcport =+ 1
+        try:
+            scan = sr1(IP(dst = sys.argv[1])/TCP(sport = srcport, dport = dstport, flags = "PFU"), verbose = 0, timeout = 0.05)
+            if scan == None:
+                print(f"{dstport} : Open | Filtered")
+            else: 
+                continue
+        except KeyboardInterrupt:
+            quit("\nKeyboard Interrupt, exiting!")
 
 def tcpFullConnectScan():
     host = sys.argv[1]
@@ -77,5 +102,6 @@ def tcpFullConnectScan():
 
 #pingSweep()
 #tcpFullConnectScan()
-tcpSynStealthScan()
-
+#tcpSynStealthScan()
+#tcpXmasScan()
+hostnameSweep()
