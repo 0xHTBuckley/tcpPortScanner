b743407aa8c6260091f382d14e793b3ba9dcdc29
Harrison Thompson Buckley <harrisonthompsonbuckley@protonmail.com>
2022-05-19 23:07:17 +0800

    Switched from using the subprocess library for the ping to a scapy library equivalent.

--- src/sweepFunctions/pingSweep.py
+++ src/sweepFunctions/pingSweep.py
@@ -1,10 +1,12 @@
 from ipaddress import ip_network
-from scapy.all import sr1, IP, ICMP
+from scapy.all import srp, ARP, Ether
 
 def pingSweep(host): 
+    print("ACTIVE HOSTS")
     hostIP = ip_network(host, strict=False)
     for addr in hostIP:
-            # Returns a 0 on success | 1 on failure
-        ping = sr1(IP(dst=addr)/ICMP(),verbose = 0, timeout = 0.03)
-        if ping == 0:
-            print(f"{addr} : online") 
+        ping = srp(Ether(dst = "ff:ff:ff:ff:ff:ff") / ARP(pdst = str(addr)), verbose = False, retry = 0, timeout = 0.3)[0]
+        if ping:
+            print(addr)
+                
+            
